b94cf8e0951f795151a8bb76d789995dcf8a986c
Mateusz Trybuła <zerq1998@gmail.com>
2022-12-04 13:25:41 +0100

    Add solution to day 1 task

---
+++ AdventOfCode.Task1/Logic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Task1
+{
+    public static class Logic
+    {
+        public static List<string> GetLinesFromFile()
+        {
+            var text = File.ReadAllLines("InputFile.txt");
+            return text.ToList();
+        }
+
+        public static List<double> GetCaloriesSum(List<string> inputData)
+        {
+            var calories = new List<double>();
+            var sum = 0d;
+
+            foreach(var calorie in inputData)
+            {
+                if (calorie.Trim() != "")
+                {
+                    var convertedVal = Convert.ToDouble(calorie);
+                    sum += convertedVal;
+                    continue;
+                }
+                calories.Add(sum);
+                sum = 0;
+            }
+
+            return calories;
+        }
+
+        public static double GetMaxCalories(List<double> calorieList)
+        {
+            return calorieList.Max();
+        }
+
+        public static double GetSumOfTopThree(List<double> calorieList)
+        {
+            return calorieList.OrderByDescending(x=>x).Take(3).Sum();
+        }
+    }
+}
---
+++ AdventOfCode.Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventOfCode.Task1;
+
+var lines = Logic.GetLinesFromFile();
+var calories = Logic.GetCaloriesSum(lines);
+var highestCalories = Logic.GetMaxCalories(calories);
+var top3 = Logic.GetSumOfTopThree(calories);
+Console.WriteLine("Elf with most calories in snacks: "+highestCalories.ToString());
+Console.WriteLine("Sum of top 3 calories in snacks: " + top3.ToString());
