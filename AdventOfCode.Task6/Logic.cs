97847ab1960c9c2988459829b4b22e4b5eb4b869
Mateusz Trybuła <zerq1998@gmail.com>
2022-12-06 21:02:36 +0100

    Add solution to day 6 task

---
+++ AdventOfCode.Task6/Logic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Task6
+{
+    public static class Logic
+    {
+        public static string GetTextFromFile()
+        {
+            return File.ReadAllText("InputFile.txt");
+        }
+
+        public static int GetMarker(string text,int numberToContains)
+        {
+            int numberOfChar = numberToContains;
+            for(int i = 0; i < numberOfChar; i++)
+            {
+                if (CheckIfCharsRepeating(text.Substring(i, numberToContains)))
+                {
+                    break;
+                }
+                numberOfChar++;
+            }
+
+            return numberOfChar;
+        }
+
+        private static bool CheckIfCharsRepeating(string text)
+        {
+            var temp = "";
+            foreach(var letter in text)
+            {
+                if (temp.Contains(letter))
+                {
+                    return false;
+                }
+                temp += letter;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AdventOfCode.Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode.Task6;
+
+var input = Logic.GetTextFromFile();
+var marker = Logic.GetMarker(input,4);
+Console.WriteLine("Chars before marker: " + marker);
+marker = Logic.GetMarker(input, 14);
+Console.WriteLine("Chars before message: " + marker);
